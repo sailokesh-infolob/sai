7dc2d41f173fbca808f21b4e4c564090365135ac
sailokesh-infolob <130435710+sailokesh-infolob@users.noreply.github.com>
2023-04-17 15:14:40 +0530

    get commit

--- sai.java
+++ sai.java
@@ -1,4 +1,5 @@
 public class Simple{
-public static void main(String args[]){
-System.out.println("hello javatpoint");
-}}
+  public static void main(String args[]){
+    System.out.println("hello javatpoint");
+  }
+}
