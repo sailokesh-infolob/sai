e94e7a0b3a100b459143f6765c7b805c2eb81c50
sailokesh-infolob <130435710+sailokesh-infolob@users.noreply.github.com>
2023-04-17 16:56:26 +0530

    Update sai.java

--- sai.java
+++ sai.java
@@ -1,5 +1,21 @@
-public class Simple {
-  public static void main(final String[] args) {
-    System.out.println("hello javatpoint");
+// Java program to demonstrate accessing of members when
+// corresponding classes are imported and not imported.
+import java.util.Vector;
+
+public class ImportDemo
+{
+  public ImportDemo()
+  {
+    // java.util.Vector is imported, hence we are
+    // able to access directly in our code.
+    Vector newVector = new Vector();
+    // java.util.ArrayList is not imported, hence
+    // we were referring to it using the complete
+    // package.
+    java.util.ArrayList newList = new java.util.ArrayList();
+  }
+  public static void main(String arg[])
+  {
+    new ImportDemo();
   }
 }
